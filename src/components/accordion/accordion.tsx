24fd3b3bd13e94669b6479d1746ce8aa8ddc284e
sonpablo <pablorodriguez833@gmail.com>
2025-12-27 18:25:22 +0100

    chore: test husky

--- src/components/accordion/accordion.tsx
+++ src/components/accordion/accordion.tsx
@@ -3,7 +3,6 @@
 import * as React from 'react';
 import * as AccordionPrimitive from '@radix-ui/react-accordion';
 import { ChevronDownIcon } from 'lucide-react';
-import {Button} from '../button/button';
 
 import { cn } from '@/lib/utils';
 
